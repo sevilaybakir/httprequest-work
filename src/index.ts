17a925bb50c51115b75da29f8a1220c658f5426c
Sevilay Bakir <sevilaybakr@hotmail.com>
2024-01-04 13:42:43 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+function fetchProduct(){
+    let product = document.getElementById("product")
+    var inputElement = document.getElementById('myInput');
+    var inputValue = inputElement.value
+   
+    let xhr = new XMLHttpRequest();
+    const url = `https://dummyjson.com/products/${inputValue}`
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            const response = xhr.responseText;
+            const jsonResponse = JSON.parse(response);
+            let listItem = `
+                <td>${jsonResponse.id}</td>
+                <td>${jsonResponse.title}</td>
+                <td>${jsonResponse.price}</td>`
+            product?.innerHTML = product?.innerHTML+listItem
+        }
+    }
+
+    xhr.send()
+
+}
